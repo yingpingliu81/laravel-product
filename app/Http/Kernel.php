859f1a01bc444ba327ee48778569a7c555f149e7
Liu Louis <lypk21@gmail.com>
2021-09-15 21:10:06 +1000

    force https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,9 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            //force https
+            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
