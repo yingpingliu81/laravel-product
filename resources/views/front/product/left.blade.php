fd550b0a1e395a8bbe5342fa5e9d8bb4c8dcf715
Liu Louis <louis.facedao@gmail.com>
2023-08-01 16:45:07 +1000

    hide discontinue

--- resources/views/front/product/left.blade.php
+++ resources/views/front/product/left.blade.php
@@ -1,7 +1,7 @@
 <div class="row product-left">
     <ul >
         @foreach($cates as $item)
-            <li class="product-left-has-sub">
+            <li class="product-left-has-sub @if($item->slug == 'discontinued-items') hide @endif">
                 <a href="{{url($item->slug)}}" class="{{Request::is($item->slug) || Request::is('*'.$item->slug.'*') ? 'active' : ''}}">{{$item->title}}</a>
             </li>
         @endforeach
