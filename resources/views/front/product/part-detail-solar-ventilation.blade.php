b9daade48b530e0bc2fb41fd0376ca7535994d0f
Liu Louis <louis.facedao@gmail.com>
2024-02-21 00:40:48 +1100

    fix

--- resources/views/front/product/part-detail-solar-ventilation.blade.php
+++ resources/views/front/product/part-detail-solar-ventilation.blade.php
@@ -119,7 +119,7 @@
 
                 <div class="col-xl-6  mt-2">
                     <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/j4PlM51Guz0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+                        <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/Dbv8BxWHjnY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
                     </div>
                 </div>
             </div>
